b7d05af7ad354ddb03b01b965fe23205a90fa093
inuki42 <inuki42@gmail.com>
2014-06-07 18:14:49 -0400

    object classes reworked

--- Fearsome_OrderSystem/src/OrderSystem_Classes/Product.java
+++ Fearsome_OrderSystem/src/OrderSystem_Classes/Product.java
@@ -6,27 +6,82 @@ package OrderSystem_Classes;
 
 /**
  *
- * @author Bella Belova
+ * @author Amy Roberts
  */
-public class Product {
-    
-    private int ProductID;
-    private int Category_ID;
-    private String ProductName;
-    private String ProductDescription;
-    private float Price;
+public class Product
+{   
+    private int productID;
+    private int categoryID;
+    private String productName;
+    private String productDesc;
+    private float productPrice;
 
-    
-
-
-    // Constructor
-    
-    public Product (int ProdID, int CategID, String ProdName, String ProdDesc, float ProdPrice) {
-        
-        ProductID = ProdID;
-        Category_ID = CategID;
-        ProductName = ProdName;
-        ProductDescription = ProdDesc;
-        Price = ProdPrice;
-    }
-}
+	 // Constructors
+	 public Product()
+	 {
+		 productID = categoryID = 0;
+		 productName = productDesc = "";
+		 productPrice = 0;
+	 } //end constructor
+	 
+    public Product (int ProdID, int CategID, String ProdName, String ProdDesc, float ProdPrice)
+	 {
+        productID = ProdID;
+        categoryID = CategID;
+        productName = ProdName;
+        productDesc = ProdDesc;
+        productPrice = ProdPrice;
+    } //end constructor
+	 
+	 public int getProductID()
+	 {
+		 return productID;
+	 } //end getProductID
+	 
+	 public void setProductID(int prodID)
+	 {
+		 productID = prodID;
+	 } //end setProductID
+	 
+	 public int getCategoryID()
+	 {
+		 return categoryID;
+	 } //end getCategoryID
+	 
+	 public void setCategoryID(int catID)
+	 {
+		 categoryID = catID;
+	 } //end setCategoryID
+	 
+	 public String getProductName()
+	 {
+		 return productName;
+	 } //end getProductName
+	 
+	 public void setProductName(String prodname)
+	 {
+		 productName = prodname;
+	 } //end setProductName
+	 
+	 public String getProductDesc()
+	 {
+		 return productDesc;
+	 } //end getProductDesc
+	 
+	 public void setProductDesc(String proddesc)
+	 {
+		 productDesc = proddesc;
+	 } //end setProductDesc
+	 
+	 public float getProductPrice()
+	 {
+		 return productPrice;
+	 } //end getProductPrice
+	 
+	 public void setProductPrice(float price)
+	 {
+		 productPrice = price;
+	 } //end setProductPrice
+	 
+	 
+} //end class
