edc4c18051a12a00f73f46c36fad74131cfa08d3
inuki42 <inuki42@gmail.com>
2014-06-07 14:34:31 -0400

    Main menu and browse menu initial switch logic working

--- Fearsome_OrderSystem/src/DB_Connection/main_class.java
+++ Fearsome_OrderSystem/src/DB_Connection/main_class.java
@@ -4,6 +4,9 @@
  */
 package DB_Connection;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import Msg_Displays.Menu;
 /**
  *
  * @author Amy Roberts
@@ -12,14 +15,56 @@ public class main_class
 {
 	public static void main(String[] args)
 	{
+		//variables
+		String input;
+		char choice;
+		boolean repeat = true;
+		
 		//initialize database connections
 		//initialize tables
 		//load data to tables
-		System.out.println("Welcome to the store, James!\n");
-		Msg_Displays.Menu.main_menu();
 		
-		//take input
-		//switch on input
+		//display main menu
+		System.out.println("Welcome to the store, James!\n");
+		Menu.main_menu();
+		
+		//take user input and switch based on choice
+		try
+		{
+			while(repeat = true)
+			{
+				BufferedReader brin = new BufferedReader(new InputStreamReader(System.in));
+				input = brin.readLine();
+				choice = input.charAt(0);
+				
+				System.out.println(choice);
+				switch(choice)
+				{
+					case '1':
+						Menu.browse_menu();
+						break;
+					case '2':
+						Menu.view_cart();
+						break;
+					case '3':
+						Menu.place_order();
+						break;
+					case 'X':
+						System.out.println("Goodbye! Thank you for shopping with us!");
+						repeat = false;
+						System.exit(0);
+						break;
+					default:
+						System.out.println("Sorry, invalid selection. Please select an option from the menu above: ");
+						break;
+				} //end switch
+			} //end while loop
+		} //end try
+		catch (Exception e)
+		{
+			System.err.println("Error: " + e);
+		} //end catch
+		
 		
 	}
 }
